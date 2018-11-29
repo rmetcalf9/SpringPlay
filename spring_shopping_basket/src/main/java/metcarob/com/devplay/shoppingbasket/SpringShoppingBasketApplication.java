f3ccf8476d5ba3f42249e60dc905a7cdd4026190
Robert Metcalf <rmetcalf9@googlemail.com>
2018-11-29 09:28:55 +0000

    Creating shopping basket example

---
+++ spring_shopping_basket/src/main/java/metcarob/com/devplay/shoppingbasket/SpringShoppingBasketApplication.java
@@ -0,0 +1,12 @@
+package metcarob.com.devplay.shoppingbasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShoppingBasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShoppingBasketApplication.class, args);
+	}
+}
---
+++ spring_shopping_basket/src/test/java/metcarob/com/devplay/shoppingbasket/SpringShoppingBasketApplicationTests.java
@@ -0,0 +1,16 @@
+package metcarob.com.devplay.shoppingbasket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringShoppingBasketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
